991dda0981c889a2e4eaf88051bbc7de39633467
Umanga Shrestha <umangashrestha93@gmail.com>
2023-08-21 16:59:55 +0545

    added filter button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, ScrollView } from "react-native";
+import { View, Text, TextInput, ScrollView, Pressable } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
@@ -6,6 +6,7 @@ import Carousel from "../component/Carousel";
 import FoodTypes from "../component/FoodTypes";
 import QuickFood from "../component/QuickFood";
 import { StatusBar } from "expo-status-bar";
+import { Ionicons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
   return (
@@ -33,6 +34,19 @@ const HomeScreen = () => {
       <FoodTypes />
       {/**QuickFood */}
       <QuickFood />
+      {/**filter button */}
+      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+      <Pressable style={{margin: 10, flexDirection: 'row', borderWidth: 0.6,borderColor:'gray', padding: 10, width: 80, borderRadius: 50, justifyContent: 'space-between', alignItems: 'center'}}>
+        <Text style={{color:'gray'}}>filter</Text>
+        <Ionicons name="filter" size={24} color="black" />
+      </Pressable>
+      <Pressable style={{margin: 10, flexDirection: 'row', borderWidth: 0.6,borderColor:'gray', padding: 10, width: 120, borderRadius: 50, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{color:'gray'}}>Sort by rating</Text>
+      </Pressable>
+      <Pressable style={{margin: 10, flexDirection: 'row', borderWidth: 0.6,borderColor:'gray', padding: 10, width: 120, borderRadius: 50, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{color:'gray'}}>Sort by price</Text>
+      </Pressable>
+      </View>
       
     </ScrollView>
     </>
