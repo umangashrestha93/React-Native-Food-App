ed3edec546c83ebdd37e2e36aaecf27e4df0701a
Umanga Shrestha <umangashrestha93@gmail.com>
2023-08-22 12:40:40 +0545

    added navigation to home screen

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import HomeScreen from './screens/HomeScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { Octicons, FontAwesome5 } from '@expo/vector-icons';
+import Profile from './screens/Profile'
+
+
+const Tab = createMaterialBottomTabNavigator()
+
+function MaterialTab(){
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name='HomeScreen' component={HomeScreen} options={{
+        tabBarIcon:()=>(<Octicons name="home" size={24} color="black" />)
+      }}/>
+      <Tab.Screen name='Profile' component={Profile} options={{
+        tabBarIcon:()=>(<FontAwesome5 name="user" size={24} color="black" />)
+      }}/>
+    </Tab.Navigator>
+  )
+}
+
+const Stack = createStackNavigator()
+
+function StackNavigator(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={MaterialTab} options={{headerShown: false}}/>
+    </Stack.Navigator>
+  )
+}
+
 
 const App = () => {
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
-      <HomeScreen />
+      <NavigationContainer>
+            <StackNavigator />
+      </NavigationContainer>
     </View>
   )
 }
